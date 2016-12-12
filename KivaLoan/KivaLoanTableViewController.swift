11e60b3ff362531de3b49c63305b270f40de7a40
A.Fairiz <afairiz@gmail.com>
2016-12-13 02:11:30 +0800

    JSON processing done

--- KivaLoan/KivaLoanTableViewController.swift
+++ KivaLoan/KivaLoanTableViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class KivaLoanTableViewController: UITableViewController {
+    
+    let kivaLoanURL = "https://api.kivaws.org/v1/loans/newest.json"
+    var loans = [Loan]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +23,60 @@ class KivaLoanTableViewController: UITableViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func getLatestLoans(){
+        
+        guard let loanUrl = URL(string: kivaLoanURL) else {
+            return
+        }
+        
+        let request = URLRequest(url: loanUrl)
+        let task = URLSession.shared.dataTask(with: request, completionHandler: {
+            (data, response, error) -> Void in
+            if let error = error{
+                print(error)
+                return
+            }
+            
+            // Parse JSON data
+            if let data = data{
+                self.loans = self.parseJSONData(data: data)
+                // reload tableview
+                OperationQueue.main.addOperation({
+                    self.tableView.reloadData()
+                })
+            }
+        
+        
+        })
+        
+        task.resume()
+    }
+    
+    func parseJSONData(data: Data) -> [Loan]{
+        var loans = [Loan]()
+        
+        do {
+            let jsonResult = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary
+            
+            // Parse JSON data
+            let jsonLoans = jsonResult?["loans"] as! [AnyObject]
+            for jsonLoan in jsonLoans {
+                let loan = Loan()
+                loan.name = jsonLoan["name"] as! String
+                loan.amount = jsonLoan["loan_amount"] as! Int
+                loan.use = jsonLoan["use"] as! String
+                let location = jsonLoan["location"] as! [String:AnyObject]
+                loan.country = location["country"] as! String
+                loans.append(loan)
+            }
+            
+        } catch {
+            print(error)
+        }
+        
+        return loans
     }
 
     // MARK: - Table view data source
---
+++ KivaLoan/Loan.swift
@@ -0,0 +1,18 @@
+//
+//  Loan.swift
+//  KivaLoan
+//
+//  Created by Ahmad Fairiz on 12/12/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import Foundation
+
+class Loan{
+    
+    var name:String = ""
+    var country: String = ""
+    var use: String = ""
+    var amount: Int = 0
+    
+}
